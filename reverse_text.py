aa475e420c161bd8da8a3e013c6db64119261924
Robert Camp s3909817 <s3909817@student.rmit.edu.au>
2023-02-27 12:46:30 +1100

    Reverse text

---
+++ reverse_text.py
@@ -0,0 +1,18 @@
+def wordReverse(word):
+    array = []
+    for i in word:
+        array.append(i)
+    array.reverse()
+    newWord = ""
+    for i in array:
+        newWord=newWord+i
+    return newWord
+
+def main():
+    word = ""
+    while len(word)==0:
+        print("Please enter a word")
+        word=input().strip()
+    print(wordReverse(word))
+
+main()
