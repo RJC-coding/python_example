235f5a09997eb4975fdd07bab85244ab19d848ec
Robert Camp s3909817 <Robert.James.Camp@gmail.com>
2023-09-04 07:57:33 +1000

    Loop for link opener

--- link_opener.py
+++ link_opener.py
@@ -2,24 +2,29 @@
 
 import webbrowser
 
+complete=False;
+
 # Passing firefox executable path to the
 # Mozilla class.
 firefox = webbrowser.Mozilla("C:\\Program Files\
 \Mozilla Firefox\\firefox.exe")
 
-print("Please enter a file to open links from.")
+while complete==False:
+    print("Please enter a file to open links from.")
 
-fileName = input()
+    fileName = input()
 
-try:
-#file = open("links.txt")
-    file = open(fileName)
-    for link in file:
-        if (link[0:3]=="www"):
-            firefox.open(link)
+    try:
+    #file = open("links.txt")
+        file = open(fileName)
+        for link in file:
+            if (link[0:3]=="www"):
+                firefox.open(link)
+        complete=True;
 
-except:
-    print("Could not find file.")
-  
-# Using open() function to display the URL.
-#firefox.open(link)
+    except:
+        print("Could not find this file.")
+      
+    # Using open() function to display the URL.
+    #firefox.open(link)
+
