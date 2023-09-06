4ca2a311fccf249678e75fa4c59d91a65654c6b8
Robert Camp s3909817 <Robert.James.Camp@gmail.com>
2023-09-07 06:00:10 +1000

    Factors

---
+++ factors.py
@@ -0,0 +1,21 @@
+running=True
+
+while (running):
+    print("Enter number or q to exit.")
+    entry=input()
+    if (entry=='q'):
+        running=False
+    else:
+        try:
+            numberEnter=int(entry)
+            if (numberEnter>0):
+                i=numberEnter
+                j=1
+                while(j<=i):
+                    if (i%j==0):
+                        print(j)
+                    j+=1
+        except:
+            print("Not a real number.")
+
+
