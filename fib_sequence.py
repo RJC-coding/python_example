366d63ce857a52e26bd3c2d33de5dcf34beec9de
Robert Camp s3909817 <Robert.James.Camp@gmail.com>
2023-09-11 13:13:38 +1000

    More fibonacci sequence

--- fib_sequence.py
+++ fib_sequence.py
@@ -1,11 +1,17 @@
 a=0
 b=1
 i=0
-upTo=10
-print("Fibonacci sequence up to " + str(upTo) + "\n")
-while (i < upTo):
-    print(a)
-    temp=a+b
-    a=b
-    b=temp
-    i+=1
+print("Fibonacci sequence")
+print("Enter a number.")
+#number=
+try:
+    upTo=int(input())
+    print("Fibonacci sequence up to " + str(upTo) + "\n")
+    while (i < upTo):
+        print(a)
+        temp=a+b
+        a=b
+        b=temp
+        i+=1
+except:
+    print("Could not use this number.")
