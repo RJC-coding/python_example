f407924f2132a96d824e732e9981ca7f621486ab
Robert Camp s3909817 <Robert.James.Camp@gmail.com>
2023-09-26 13:22:50 +1000

    Reverse word loop

--- reverse_text.py
+++ reverse_text.py
@@ -9,10 +9,15 @@ def wordReverse(word):
     return newWord
 
 def main():
-    word = ""
-    while len(word)==0:
-        print("Please enter a word to reverse")
-        word=input().strip()
-    print(wordReverse(word))
+    complete=False
+    while (complete==False):
+        word = ""
+        while len(word)==0:
+            print("Please enter a word to reverse. Enter q to exit.")
+            word=input().strip()
+            if (word=="q"):
+                complete=True
+            else:
+                print(wordReverse(word))
 
 main()
